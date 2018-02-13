da66f9791283db75f6267218e2ef09ed9f23d3be
qchenevier <qchenevier@users.noreply.github.com>
2018-02-13 23:08:17 +0100

    removed bugged bash script. it's better to use aws cli to configure and test the keys

--- source_accessKeys_to_env.sh
+++
@@ -1,8 +0,0 @@
-
-# read access keys
-access_keys_file=$(tail accessKeys.csv -n 1)
-IFS=\, read -a access_keys <<< "$access_keys_file" # IFS to split string on ','
-
-# export env variables
-export AWS_ACCESS_KEY_ID=${access_keys[0]}
-export AWS_SECRET_ACCESS_KEY=${access_keys[1]}

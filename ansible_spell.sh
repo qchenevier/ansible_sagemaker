82a0e4f802742a31e0781bfb32c6c37632e06a99
qchenevier <qchenevier@users.noreply.github.com>
2018-02-19 00:09:20 +0100

    updated spells and added scripts

--- ansible_spell.sh
+++ ansible_spell.sh
@@ -4,3 +4,5 @@ ansible-playbook -i /etc/ansible/ec2.py print_facts.yml
 ansible-playbook -i ec2.py print_facts.yml
 
 ansible-playbook -i ec2.py print_facts.yml --extra-vars "state_filter=['stopped']"
+
+ansible-playbook -i ec2.py install_jupyter_notebook.yml --private-key "./tmp_key.pem"

836b7a77e8b64d56fc0d8b2ee7f73111e9582970
qchenevier <qchenevier@users.noreply.github.com>
2018-02-21 07:45:18 +0100

    updated tasks

--- ansible_spell.sh
+++ ansible_spell.sh
@@ -3,6 +3,8 @@ ansible-playbook gather_facts.yml
 ansible-playbook -i /etc/ansible/ec2.py print_facts.yml
 ansible-playbook -i ec2.py print_facts.yml
 
-ansible-playbook -i ec2.py print_facts.yml --extra-vars "state_filter=['stopped']"
+ansible-playbook -i ec2.py print_facts.yml --extra-vars "state=['stopped']"
+
+ansible-playbook -i ec2.py start_stopped_instance.yml
 
 ansible-playbook -i ec2.py install_jupyter_notebook.yml --private-key "./tmp_key.pem"

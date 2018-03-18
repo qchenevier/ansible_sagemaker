a1719107e6f3c98415ccc49c62cffb7b744fe909
qchenevier <qchenevier@users.noreply.github.com>
2018-03-18 01:01:31 +0100

    updated spells

--- ansible_spells.sh
+++ ansible_spells.sh
@@ -1,16 +1,5 @@
-# installation (run only once)
-ansible-playbook -i ec2.py create_key.yml
-ansible-playbook -i ec2.py create_instance.yml
-
-# start
-ansible-playbook -i ec2.py list_instances.yml
-ansible-playbook -i ec2.py start_stopped_instances.yml
-ansible-playbook -i ec2.py add_localhost_IP_to_default_rules.yml
-ansible-playbook -i ec2.py start_jupyter_and_create_tunnel.yml
-
-# stop (run only one of the two options)
-ansible-playbook -i ec2.py stop_instances.yml
-ansible-playbook -i ec2.py stop_instances_and_tunnels.yml
+ansible-playbook -i ec2.py deep_learning_start.yml
+ansible-playbook -i ec2.py deep_learning_stop.yml
 
 # mount remote file system locally
-sudo sshfs -o allow_other,default_permissions,IdentityFile="$(pwd)/tmp_key.pem" ubuntu@ec2-34-244-108-241.eu-west-1.compute.amazonaws.com:/home/ubuntu "$(pwd)/shared_fs_test/"
+sudo sshfs -o allow_other,default_permissions,IdentityFile="$(pwd)/my_ssh_key.pem" ubuntu@ec2-34-244-108-241.eu-west-1.compute.amazonaws.com:/home/ubuntu "$(pwd)/shared_fs_test/"

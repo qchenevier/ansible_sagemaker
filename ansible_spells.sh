f053524cef8b7a98278628363ad1e1d1bae66e7b
qchenevier <qchenevier@users.noreply.github.com>
2018-03-25 16:21:35 +0200

    simplified spells

--- ansible_spells.sh
+++ ansible_spells.sh
@@ -1,5 +1,5 @@
-ansible-playbook -i ec2.py deep_learning_start.yml
-ansible-playbook -i ec2.py deep_learning_stop.yml
+ansible-playbook deep_learning_start.yml
+ansible-playbook deep_learning_stop.yml
 
 # mount remote file system locally
 sudo sshfs -o allow_other,default_permissions,IdentityFile="$(pwd)/my_ssh_key.pem" ubuntu@ec2-34-244-108-241.eu-west-1.compute.amazonaws.com:/home/ubuntu "$(pwd)/shared_fs_test/"
